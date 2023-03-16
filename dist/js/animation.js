70d8e5f57b28b9f13a54f9fde8619da0b26eb65f
Oksana <oksana.100@mail.ru>
2023-03-16 20:05:58 +0300

    fix: gallery card

--- dist/js/animation.js
+++ dist/js/animation.js
@@ -1,25 +1,28 @@
-export function animation(entry){
-   if (entry) entry.forEach(change => {
-      if(change.isIntersecting){
-         change.target.classList.add('el-show');
+export function animation(entry) {
+  if (entry)
+    entry.forEach((change) => {
+      if (change.isIntersecting) {
+        change.target.classList.add("el-show");
       } else {
-         change.target.classList.remove('el-show');
+        change.target.classList.remove("el-show");
       }
-   });
+    });
 }
 
-let options = {threshold: [0.5]};
+let options = { threshold: [0.5] };
 let observer = new IntersectionObserver(animation, options);
-let elements = document.querySelectorAll('.animation-x-l');
-let elements2 = document.querySelectorAll('.animation-x-r');
-let elements3 = document.querySelectorAll('.animation-y');
 
-for(let el of elements){
-   observer.observe(el);
-}
-for(let el of elements2){
-   observer.observe(el);
-}
-for(let el of elements3){
-   observer.observe(el);
+const elements = [
+  ...document.querySelectorAll(".animation-x-l"),
+  ...document.querySelectorAll(".animation-x-r"),
+  ...document.querySelectorAll(".animation-y"),
+  ...document.querySelectorAll(".animation-about"),
+  ...document.querySelectorAll(".about__photo-1"),
+  ...document.querySelectorAll(".about__photo-2"),
+  ...document.querySelectorAll(".about__photo-3"),
+  ...document.querySelectorAll(".about__photo-4")
+];
+
+for (let el of elements) {
+  observer.observe(el);
 }
