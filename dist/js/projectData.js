8b002ee2b75a291ce03eb0fd4f261d8a4b0e042c
Oksana <oksana.100@mail.ru>
2023-03-16 20:25:28 +0300

    fix: photo files

--- dist/js/projectData.js
+++ dist/js/projectData.js
@@ -1,28 +1,28 @@
 export const projectData = [
    {
       name: 'Shelter',
-      image: 'assets/img/shelter.png',
+      image: 'assets/img/shelter.jpg',
       link: 'https://ksenchik.github.io/Shelter/shelter/pages/main/',
       description: 'Adaptive layout, Pixel perfect, HTML, CSS, JS, jQuery',
       code: 'https://github.com/Ksenchik/Shelter/tree/main/shelter'
    },
    {
       name: 'Online-zoo',
-      image: 'assets/img/online-zoo.png',
+      image: 'assets/img/online-zoo.jpg',
       link: 'https://ksenchik.github.io/PetStory-online/online-zoo/pages/main/',
       description: 'Adaptive layout, Pixel perfect, HTML, SCSS, JS, jQuery, validation HTML form, JS functional',
       code: 'https://github.com/Ksenchik/PetStory-online/tree/main/online-zoo'
    },
    {
       name: 'Songbird',
-      image: 'assets/img/songbird.png',
+      image: 'assets/img/songbird.jpg',
       link: 'https://rolling-scopes-school.github.io/ksenchik-JSFE2022Q3/songbird/dist/',
       description: 'Adaptive layout, custom design, HTML, SCSS, JS, SPA routing, game logic',
       code: 'https://github.com/Ksenchik/Songbird/tree/main/songbird'
    },
    {
       name: 'Online-store',
-      image: 'assets/img/online-store.png',
+      image: 'assets/img/online-store.jpg',
       link: 'https://rolling-scopes-school.github.io/ksenchik-JSFE2022Q3/songbird/dist/',
       description: 'Adaptive layout, HTML, SCSS, JS',
       code: 'https://github.com/Ksenchik/Online-store/tree/develop/public/template'
