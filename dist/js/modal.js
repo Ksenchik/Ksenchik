a96e748c5d4b89130706ff3701390286b8a1323b
Oksana <oksana.100@mail.ru>
2023-03-20 14:16:32 +0300

    fix: modal functionsl

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -1,21 +1,26 @@
+import { create } from "./create.js";
 import { projectData } from "./projectData.js";
 
 export function modalOpen() {
   const modal = document.querySelector(".modal");
   const btn = document.querySelector(".portfolio__gallery-see-all");
   const span = document.querySelector(".modal__close");
+  const contentBody = document.querySelector("body");
 
   btn.onclick = function () {
     modal.style.display = "block";
+    contentBody.classList.toggle("lock");
   };
 
   span.onclick = function () {
     modal.style.display = "none";
+    contentBody.classList.remove("lock");
   };
 
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      contentBody.classList.remove("lock");
     }
   };
 }
@@ -24,28 +29,17 @@ let container = document.querySelector(".modal__projects");
 
 export function modalWrite() {
   for (let i = 0; i < projectData.length; i++) {
-    let item = document.createElement("div");
-    let title = document.createElement("h6");
-    let a = document.createElement("a");
-    let img = document.createElement("img");
-    let p = document.createElement("p");
-    item.classList.add("modal__projects-item");
-    title.classList.add("modal__projects-name");
-    a.classList.add("modal__projects-link");
-    a.setAttribute("href", "somelink url");
+    const item = create("div", "modal__projects-item");
+    const title = create("h6", "modal__projects-name", {innerHTML: projectData[i].name,});
+    const a = create("a", "modal__projects-link", {href: projectData[i].link,});
     a.setAttribute("target", "_blank");
-    img.classList.add("modal__projects-img");
-    img.setAttribute("src", "searchPic");
-    p.classList.add("modal__projects-description");
+    const img = create("img", "modal__projects-img", {src: projectData[i].image,});
+    const p = create("p", "modal__projects-description", {innerHTML: projectData[i].description,});
 
     container.append(item);
     item.append(title);
-    title.innerHTML = projectData[i].name;
     item.append(a);
-    a.href = projectData[i].link;
     a.append(img);
-    img.src = projectData[i].image;
     item.append(p);
-    p.innerHTML = projectData[i].description;
   }
 }
