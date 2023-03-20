5053dc102b7971644a473fa71f5dcd4842c66ac2
Oksana <oksana.100@mail.ru>
2023-03-20 23:18:35 +0300

    fix: slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,17 @@
 import { animation } from "./animation.js";
 import { burger } from "./burger.js";
 import { modalOpen, modalWrite } from "./modal.js";
-import { galleryLeft, galleryRight } from "./slider.js";
+import { createSlider } from "./slider.js";
+// import { galleryLeft, galleryRight } from "./slider.js";
 
 function app() {
   burger();
-  galleryRight();
-  galleryLeft();
+//   galleryRight();
+//   galleryLeft();
   animation();
   modalOpen();
   modalWrite();
+  createSlider();
 }
 
 
--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,42 +1,105 @@
 import { projectData } from "./projectData.js";
+import { create } from "./create.js";
 
-let order = 0;
+// let order = 0;
 
-const left = document.querySelector(".portfolio__card-ntn-left");
-const right = document.querySelector(".portfolio__card-ntn-right");
+// const left = document.querySelector(".portfolio__card-ntn-left");
+// const right = document.querySelector(".portfolio__card-ntn-right");
 
-right.addEventListener("click", () => {
-  order++;
-  galleryRight();
-});
+// right.addEventListener("click", () => {
+//   order++;
+//   galleryRight();
+// });
 
-left.addEventListener("click", () => {
-  order--;
-  galleryLeft();
-});
+// left.addEventListener("click", () => {
+//   order--;
+//   galleryLeft();
+// });
 
-function write() {
-  document.querySelector(".portfolio__card-title").innerHTML =
-    projectData[order].name;
-  document.querySelector(".portfolio__card-img").src = projectData[order].image;
-  document.querySelector(".portfolio__card-link").href =
-    projectData[order].link;
-  document.querySelector(".portfolio__card-description").innerHTML =
-    projectData[order].description;
-  document.querySelector(".portfolio__card-code").href =
-    projectData[order].code;
+// function write() {
+//   document.querySelector(".portfolio__card-title").innerHTML =
+//     projectData[order].name;
+//   document.querySelector(".portfolio__card-img").src = projectData[order].image;
+//   document.querySelector(".portfolio__card-link").href =
+//     projectData[order].link;
+//   document.querySelector(".portfolio__card-description").innerHTML =
+//     projectData[order].description;
+//   document.querySelector(".portfolio__card-code").href =
+//     projectData[order].code;
+// }
+
+// export function galleryRight() {
+//   if (order >= projectData.length) {
+//     order = 0;
+//   }
+//   write();
+// }
+
+// export function galleryLeft() {
+//   if (order < 0) {
+//     order = projectData.length;
+//   }
+//   write();
+// }
+
+// import Swiper from '../slider/swiper-bundle.esm.browser.min.js'
+
+
+
+ const wrap = document.querySelector('.slider-line');
+
+ export function createSlider(){
+   for (let i = 0; i < projectData.length; i++){
+      const item = create('div', 'slider-item');
+      const title = create('h4', 'portfolio__card-title', {innerHTML: projectData[i].name,});
+      const wrapPhoto = create('div', 'portfolio__card-photo');
+      const aPhoto = create('a', 'portfolio__card-link', {href: projectData[i].link,});
+      aPhoto.setAttribute("target", "_blank");
+      const img = create('img', 'portfolio__card-img', {src: projectData[i].image,});
+      const pDescription = create('p', 'portfolio__card-description', {innerHTML: projectData[i].description,});
+      const aCode = create('a', 'portfolio__card-code', {href: projectData[i].code,})
+      aCode.setAttribute("target", "_blank");
+      aCode.innerHTML = 'See code &#10142;';
+
+      wrap.append(item);
+      item.append(title);
+      item.append(wrapPhoto);
+      wrapPhoto.append(aPhoto);
+      aPhoto.append(img);
+      item.append(pDescription);
+      item.append(aCode);
+   }
+ }
+
+
+ let offset = 0;
+ let widthItem = 0;
+ if(document.documentElement.clientWidth < 768){
+   widthItem = 280;
+} else{
+   widthItem = 700;
 }
 
-export function galleryRight() {
-  if (order >= projectData.length) {
-    order = 0;
-  }
-  write();
-}
+ const sliderLine = document.querySelector('.slider-line');
+ sliderLine.style.width = widthItem * projectData.length;
 
-export function galleryLeft() {
-  if (order < 0) {
-    order = projectData.length;
-  }
-  write();
-}
+ document.querySelector('.portfolio__card-ntn-right').addEventListener('click', function(){
+     
+   offset = offset + widthItem;
+   if(offset > (widthItem * (projectData.length - 1))){
+      offset = 0;
+   }
+   sliderLine.style.left = -offset + 'px';
+
+ })
+
+ document.querySelector('.portfolio__card-ntn-left').addEventListener('click', function(){
+     
+   offset = offset - widthItem;
+   if(offset < 0){
+      offset = widthItem * (projectData.length - 1);
+   }
+   sliderLine.style.left = -offset + 'px';
+
+ })
+
