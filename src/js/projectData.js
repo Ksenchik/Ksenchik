4d9acad1c80d1fe76fe81cf43288891b8ea4c389
Oksana <oksana.100@mail.ru>
2024-03-26 11:51:42 +0300

    fix: img

--- src/js/projectData.js
+++ src/js/projectData.js
@@ -8,7 +8,7 @@ export const projectData = [
    },
    {
     name: 'УОМ',
-    image: 'assets/img/uom.jpg',
+    image: 'assets/img/uom.gif',
     link: 'https://ksenchik.github.io/university/dist/',
     description: 'Adaptive layout, HTML, UI kit, SCSS, JS (burger, modal, swiper, gallery)',
     code: 'https://github.com/Ksenchik/university'
