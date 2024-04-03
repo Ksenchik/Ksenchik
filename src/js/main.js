b7ff0f936df209b79dec5242e318d4c22b8371e8
Oksana <oksana.100@mail.ru>
2024-04-03 11:28:10 +0300

    feat: add preloader

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ import { burger } from "./burger.js";
 import { modalOpen, modalWrite } from "./modal.js";
 import { createSlider } from "./slider.js";
 
+window.addEventListener('load', function() {
+  const preloader = document.querySelector('.loader');
+
+  preloader.style.display = 'none';
+});
+
 function app() {
   burger();
   animation();
